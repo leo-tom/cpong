23993c21ff2297703d2d97ea72d1b94eea4f3488
leo-tom <ttnistomura@gmail.com>
2017-02-12 14:19:36 +0900

    First commit

---
+++ CMakeFiles/libncurses.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for libncurses.
+# This may be replaced when dependencies are built.
---
+++ CMakeFiles/pong.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pong.dir/main.c.o"
+  "CMakeFiles/pong.dir/output.c.o"
+  "CMakeFiles/pong.dir/play.c.o"
+  "pong.pdb"
+  "pong"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
